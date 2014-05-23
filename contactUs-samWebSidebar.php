d056668bc948228928b98974a3b1c4244de41bd0
zf72 <zf72@m08092297.nordstrom.net>
2014-05-23 14:38:51 -0700

    contactUs work assignment
    sending the user to the correct help documentation before allowing
    contact with us. (the rest of the original document lives in the SAMWeb
    folder “includes” called “samWebSidebar.php”

---
+++ contactUs-samWebSidebar.php
@@ -0,0 +1,73 @@
+<li>
+	<script>
+		if (document.URL === "studioOverview.php") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Studio%20Dashboard&title=Studio%20Overview";
+		}
+		else if (document.URL === "studioOverview.php?studio=Georgetown" || "studioOverview.php?studio=SODO" || "studioOverview.php?studio=Studio C" || "studioOverview.php?studio=New York") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Studio%20Dashboard&title=Individual%20Studio";
+		}
+		else if (document.URL === "setdetail.php") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Studio%20Dashboard&title=Set%20Detail";
+		}
+		else if (document.URL === "iptOverview.php") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=IPT%20Dashboard&title=IPT%20Overview";
+		}
+		else if (document.URL === "iptOverview.php?studio=Seattle IPT" || "iptOverview.php?studio=Seattle IPT" || "iptOverview.php?studio=SODO" || "iptOverview.php?studio=Studio C") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=IPT%20Dashboard&title=Individual%20IPT";
+		}
+		else if (document.URL === "iptdetail.php") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=IPT%20Dashboard&title=IPT%20Detail";
+		}
+		else if (document.URL === "imageReview.php") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Image%20Review";
+		}
+		else if (document.URL === "reporting.php") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Reporting";
+		}
+		else if (document.URL === "dashboard.php#tabDashboard") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=General";
+		}
+		else if (document.URL === "dashboard.php") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Vendor";
+		}
+		else if (document.URL === "vendorImages.php?status=assign") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Vendor&title=Assigning%20Images";
+		}
+		else if (document.URL === "vendorImages.php?status=pendingipt") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Vendor&title=IPT%20image%20review";
+		}
+		else if (document.URL === "vendorImages.php?status=pendingad") {
+			location.href = "https://samweb.nordstrom.net/help.php?section=Vendor&title=AD%20image%20review";
+		}
+		else {
+			location.href = "mailto:studioncst@exchange.nordstrom.com?subject=SAMWeb Support - <?=$ticket_timestamp?>&body=<?=$contactBody?>";
+		};
+	</script>
+	<a href="mailto:studioncst@exchange.nordstrom.com?subject=SAMWeb Support - <?=$ticket_timestamp?>&body=<?=$contactBody?>">
+		<i class="fa fa-envelope-o"></i>
+		<span class="title">Contact Us</span>
+		<span class="badge badge-roundless badge-success">New</span>
+	</a>
+</li>
+<!--
+
+<a href="studioOverview.php">
+<a href="studioOverview.php?studio=Georgetown">
+<a href="studioOverview.php?studio=SODO">
+<a href="studioOverview.php?studio=Studio C">
+<a href="studioOverview.php?studio=New York">
+
+<a href="historySearch.php">
+
+<a href="iptOverview.php">
+<a href="iptOverview.php?studio=Seattle IPT">
+<a href="iptOverview.php?studio=SODO">
+<a href="iptOverview.php?studio=Studio C">
+<a href="imageReview.php">
+<a href="reporting.php">
+<a href="dashboard.php">
+<a href="releaseNotes.php?pg=web">
+<a href="releaseNotes.php?pg=app">
+<a href="help.php">
+
+	-->
